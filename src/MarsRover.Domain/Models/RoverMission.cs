4b3c10f17019ce82446ee811c4dd998aad950713
Luiz Felipe Mandetta Clementino <lfclementino@gmail.com>
2021-07-16 20:20:34 +0200

    Add RoverMission model

---
+++ src/MarsRover.Domain/Models/RoverMission.cs
@@ -0,0 +1,16 @@
+﻿namespace MarsRover.Domain.Models
+{
+    public class RoverMission
+    {
+        public RoverMission()
+        { }
+
+        public RoverMission(Floor floor, Rover rover)
+        {
+            Floor = floor;
+            Rover = rover;
+        }
+        public Floor Floor { get; set; }
+        public Rover Rover { get; set; }
+    }
+}
---
+++ tests/MarsRover.Tests/Models/RoverMissionTests.cs
@@ -0,0 +1,35 @@
+﻿using MarsRover.Domain.Models;
+using Xunit;
+
+namespace MarsRover.Tests.Models
+{
+    public class RoverMissionTests
+    {
+        [Fact]
+        public void ShouldCreateNewModel()
+        {
+            var roverMission = new RoverMission();
+
+            Assert.NotNull(roverMission);
+        }
+
+        [Fact]
+        public void ShouldSetCorrectValuesOnNewModel()
+        {
+
+            var rover = new Rover();
+            var floor = new Floor()
+            {
+                Height = 100,
+                Width = 100
+            };
+
+            var roverMission = new RoverMission(floor, rover);
+
+            Assert.NotNull(roverMission);
+            Assert.Equal(rover.Id, roverMission.Rover.Id);
+            Assert.Equal(100, roverMission.Floor.Height);
+            Assert.Equal(100, roverMission.Floor.Width);
+        }
+    }
+}
